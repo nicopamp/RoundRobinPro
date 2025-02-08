6714bb82c072b0e60b7bf02263f2033279570791
Nico Pampaloni <nico@pampfamily.com>
2025-02-08 12:08:36 -0500

    Initial Commit

---
+++ RoundRobinPro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RoundRobinPro
+//
+//  Created by Nico Pampaloni on 2/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RoundRobinPro/RoundRobinProApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoundRobinProApp.swift
+//  RoundRobinPro
+//
+//  Created by Nico Pampaloni on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct RoundRobinProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
